21ed1beaa88f9330cd2d80b951ce9ff8023af007
Makayla <mcsyanos@hawaii.edu>
2018-02-18 22:26:10 -1000

    Completed assignment #5
    while loop and for in loop

--- swiftPlaygroundFour.playground/Contents.swift
+++ swiftPlaygroundFour.playground/Contents.swift
@@ -3,3 +3,43 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+//for number in 1...5
+//{
+    //print(number)
+//}
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print(sum)
+}
+
+var i = 1
+while i <= 10
+{
+    //we want to print i
+    print(i)
+    //we want to add +1 to i for each iteration through the loop
+    i = i + 1
+}
+
+//Coding Challenge #1
+var k = 0
+while k <= 2
+{
+    print(k)
+    k = k + 1
+}
+
+//Coding Challenge #2
+var newSum = 10
+for numbers in 1...10
+{
+    newSum -= numbers
+    print(newSum)
+}
+
+
